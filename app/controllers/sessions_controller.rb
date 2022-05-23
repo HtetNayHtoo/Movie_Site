8979b08fbdd53b1623e744c8a939bdd17cb5d936
Naing Lynn Aung <unrivaledking1@gmail.com>
2022-05-23 14:58:33 +0630

    updating user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to home_index_path, notice: "Logged in!"
+      redirect_to login_path, notice: "Logged in!"
     else
-      flash.now[:alert] = "Email or password is invalid"
-      render "new"
+      redirect_to login_path ,notice: "Email or password is invalid"
+     
     end
   end
   def destroy
     session[:user_id] = nil
-    redirect_to home_index_path, notice: "Logged out!"
+    redirect_to login_path, notice: "Logged out!"
   end
 
 end
