c4c9ca6171bc0b2fe65ef831d121e3d24dd801fa
HtetNayHtoo <hhtetnayhtoo@gmail.com>
2022-05-24 17:03:33 +0630

    favorite task

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,13 +1,33 @@
 class FavouritesController < ApplicationController
     def create
-      @movie = Movie.find(params[:movie_id])
-      if @movie.user_id != current_user.id   #You can register favorites other than your own posts
-        @favourite = Favorite.create(user_id: current_user.id, movie_id: @movie.id)
+
+      favourite = Favourite.where(movie: Movie.find(params[:movie]), user: current_user)
+      if favourite == []
+            # Create the fav
+            @favourite = Favorite.create(user_id: current_user.id, movie_id: @movie.id)
+            @favourite_exists = true
+      else
+           # Delete the fav
+           favourite.destroy_all
+           @favourite_exists = false
       end
+
+      respond_to do |format|
+        format.html {}
+        format.js {}
+
+
+      # @movie = Movie.find(params[:movie_id])
+      # if @movie.user_id != current_user.id   #You can register favorites other than your own posts
+      #   @favourite = Favourite.create(user_id: current_user.id, movie_id: @movie.id)
+      # end
     end
+
     def destroy
-      @movie = Movie.find(params[:movie_id])
-      @favourite = Favorite.find_by(user_id: current_user.id, movie_id: @movie.id)
-      @favourite.destroy
+
+      # @movie = Movie.find(params[:movie_id])
+      # @favourite = Favourite.find_by(user_id: current_user.id, movie_id: @movie.id)
+      # @favourite.destroy
+
     end
   end
