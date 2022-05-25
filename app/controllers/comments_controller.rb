314beff7972a38b5084c0df7f5dc555dd403efa6
LAPTOP-CAR9KD80\wp <hninsuhlaing2282020@gmail.com>
2022-05-25 23:29:52 +0630

    adding time and delete function in comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,11 @@ class CommentsController < ApplicationController
     end
       redirect_to movie_path(params[:movie_id])
   end
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment_id = @comment.id
+    @comment.destroy
+  end
   private 
   def comment_params
     params.require(:comment).permit(:content).merge(movie_id: params[:movie_id])
